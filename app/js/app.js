2485d652ad347ec1bd2262c299b4c7dac34be292
JacBac <jacbachelier@gmail.com>
2013-05-02 23:39:20 +0200

    contact route test

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,19 @@ var app = angular.module('VisiteApp', []);
 app.config(function($routeProvider) {
   $routeProvider
       .when('/etapes',                   { controller: 'EtapesCtrl',            templateUrl: 'partials/etapes.html' })
-      .otherwise(                       { redirectTo: '/etapes' });
+      .when('/contact',                  { controller: 'ContactCtrl',           templateUrl: 'partials/contact.html' })
+      .otherwise(                        { redirectTo: '/etapes' });
 });
 
 app.controller('EtapesCtrl', ['$http', '$scope', function($http, $scope) {
   $http.get('/server/data/etapes.json/$')
   .success(function(etapes) {
       $scope.etapes = etapes;
+  });
+
+app.controller('ContactCtrl', ['$http', '$scope', function($http, $scope) {
+  $http.get('/server/data/contact.json/$')
+  .success(function(contact) {
+      $scope.contact = contact;
   });
 }]);
